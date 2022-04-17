7069357d02a76ec4679ed338276c12c7096bb278
Harry <harryliu1995@gmail.com>
2022-04-17 15:51:03 -0400

    refator: rename to maximumSubarray.py

--- array/maximumSubarray.py
+++ array/maximumSubarray.py
@@ -2,7 +2,6 @@ class Solution:
     @classmethod
     def maxSubArray(self, nums):
         for i in range(1, len(nums)):
-            print(nums)
             if nums[i - 1] > 0:
                 nums[i] += nums[i - 1]
         return max(nums)
