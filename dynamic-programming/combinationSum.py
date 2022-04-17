967a22235024a27a90ae0b9344a1cefa639866e2
Harry <harryliu1995@gmail.com>
2022-04-16 21:57:00 -0400

    q: combination sum using dp

--- dynamic-programming/combinationSum.py
+++ dynamic-programming/combinationSum.py
@@ -1,24 +1,13 @@
 class Solution:
     @classmethod
     def combinationSum4(self, nums, target):
-        result = []
-
-        def dfs(i, current, total):
-            print(i, current, total)
-            if total == target:
-                result.append(current.copy())
-                return
-            if i >= len(nums) or total > target:
-                return
-            current.append(nums)
-            dfs(i, current, total + nums[i])
-            current.pop()
-            dfs(i + 1, current, total)
-
-        dfs(0, [], 0)
-        return result
+        dp = {0: 1}  # tracks number of permutations per value
+        for total in range(1, target + 1):
+            dp[total] = 0
+            for n in nums:
+                dp[total] += dp.get(total - n, 0)
+        return dp[target]
 
 
-res = len(Solution.combinationSum4([1, 2, 3], 4))
+res = Solution.combinationSum4([1, 2, 3], 4)
 print(res)
-
