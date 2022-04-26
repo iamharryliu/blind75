dcf2340c6dc4810a2227a1a419e2a8d27f978bef
Harry <harryliu1995@gmail.com>
2022-04-25 22:12:56 -0400

    refactor: find all anagrams in string

--- string/findAllAnagrams.py
+++ string/findAllAnagrams.py
@@ -14,9 +14,10 @@ class Solution:
         for i, c in enumerate(s):
             s_count[c] = s_count.get(c, 0) + 1
             if i >= len(p):
-                s_count[s[i - len(p)]] -= 1
-                if s_count[s[i - len(p)]] == 0:
-                    s_count.pop(s[i - len(p)])
+                firstChar = s[i - len(p)]
+                s_count[firstChar] -= 1
+                if s_count[firstChar] == 0:
+                    s_count.pop(firstChar)
             if s_count == p_count:
                 res.append(i - len(p) + 1)
         return res
@@ -24,4 +25,3 @@ class Solution:
 
 res = Solution.findAnagrams(s="cbaebabacd", p="abc")
 print(res == [0, 6])
-
