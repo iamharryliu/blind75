627cc528ef2bec4139374f6c8405b867b9ce7a5f
Harry <harry1.liu@bmo.com>
2022-05-10 14:49:15 -0400

    refactor: lowest common ancester

--- tree/lowestCommonAncestor.py
+++ tree/lowestCommonAncestor.py
@@ -10,7 +10,7 @@ class Solution:
     def lowestCommonAncestor(self, root, p, q):
         while root:
             # move root value towards p and q
-            if root.val > p.val and root.val > q.val:
+            if p.val < root.val and q.val < root.val:
                 root = root.left
             elif root.val < p.val and root.val < q.val:
                 root = root.right
