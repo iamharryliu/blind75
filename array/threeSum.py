06933ddcc4aae298d3ffc4f70e2f91bd943d99d5
Harry <harry1.liu@bmo.com>
2022-05-11 10:53:03 -0400

    refactor: 3sum cleaup

--- array/threeSum.py
+++ array/threeSum.py
@@ -4,33 +4,27 @@ class Solution:
         res = []
         nums.sort()
         for i, num in enumerate(nums):
-            # skip index if same as previous number
-            if i > 0 and num == nums[i - 1]:
+
+            if i > 0 and num == nums[i - 1]:  # skip index if same as previous number
                 continue
 
-            # set left and right indices
-            left, right = i + 1, len(nums) - 1
+            l, r = i + 1, len(nums) - 1
 
-            while left < right:
-                three_sum = num + nums[left] + nums[right]
-
-                # advance left and right indice depending on three sum value
+            while l < r:
+                three_sum = num + nums[l] + nums[r]
                 if three_sum < 0:
-                    left += 1
-                elif three_sum > 0:
-                    right -= 1
+                    l += 1
+                if three_sum > 0:
+                    r -= 1
+                if three_sum == 0:
+                    res.append((num, nums[l], nums[r]))
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:  # skip similar l values
+                        l += 1
 
-                else:
-                    # if three_sum equals 0 add to result
-                    res.append((num, nums[left], nums[right]))
-
-                    # advance left until it is not same as previous value
-                    left += 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
         return res
 
 
 test_value = [-1, -1, 0, 1, 2, -1, -4]
-result = Solution.threeSum(test_value)
+result = Solution.threeSum(test_value) == [(-1, -1, 2), (-1, 0, 1)]
 print(result)
