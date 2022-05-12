e99c20594cc4d79ea2526b052da8f50611688e16
Harry <harry1.liu@bmo.com>
2022-05-12 11:24:56 -0400

    refator: length of longest substring w/o repeating chars

--- string/lengthOfLongestSubstringWithoutRepeatingCharacers.py
+++ string/lengthOfLongestSubstringWithoutRepeatingCharacers.py
@@ -2,12 +2,10 @@ class Solution:
     @classmethod
     def lengthOfLongestSubstring(self, s: str) -> int:
         substring = ""
-        l = 0
         maxLength = 0
         for c in s:
-            while c in substring:
-                substring = substring[1:]
-                l += 1
+            if c in substring:
+                substring = substring[substring.index(c) + 1 :]
             substring += c
             maxLength = max(maxLength, len(substring))
         return maxLength
@@ -15,4 +13,4 @@ class Solution:
 
 test = "abcabcbb"
 expected_res = 3
-print(Solution.lengthOfLongestSubstring(test))
+print(Solution.lengthOfLongestSubstring(test) == expected_res)
