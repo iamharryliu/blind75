19ffc1719665647e7bb64e7ce34f240fc10c3216
Harry <harryliu1995@gmail.com>
2022-05-22 14:13:11 -0400

    refactor: rename course schedule variables

--- graph/courseSchedule.py
+++ graph/courseSchedule.py
@@ -1,31 +1,25 @@
 class Solution:
     @classmethod
     def canFinish(self, numCourses, prerequisites):
-
-        # Create map of prereqs {course: [...prereqs]} and set for visited
-        prerequisiteMap = {i: [] for i in range(numCourses)}
-        for course, prerequisite in prerequisites:
-            prerequisiteMap[course].append(prerequisite)
+        adj_list = {i: [] for i in range(numCourses)}
+        for a, b in prerequisites:
+            adj_list[a].append(b)
         visited = set()
 
         def dfs(course):
-            # base cases
             if course in visited:
                 return False
-            if prerequisiteMap[course] == []:
+            if adj_list[course] == []:
                 return True
-            # dfs course prerequestites
+
             visited.add(course)
-            for prerequisite in prerequisiteMap[course]:
-                if not dfs(prerequisite):
+            for prereq in adj_list[course]:
+                if not dfs(prereq):
                     return False
-            # done dfs so we can remove the course from the visited nodes
             visited.remove(course)
-            # completed dfs so we don't need to visit its prereqs again
-            prerequisiteMap[course] = []
+            adj_list[course] = []
             return True
 
-        # loop through courses and run dfs on them
         for course in range(numCourses):
             if not dfs(course):
                 return False
