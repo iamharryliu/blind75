afd11eae7715b422e23ffdeef50c99159ec32b4a
Harry <harryliu1995@gmail.com>
2022-05-22 14:11:31 -0400

    refactor: binary tree right side view dfs

--- tree/binaryTreeRightSideView.py
+++ tree/binaryTreeRightSideView.py
@@ -12,29 +12,28 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
 
         # DFS
-        # res = []
-        # def dfs(node, currentLevel):
-        #     nonlocal res
-        #     if not node: return
-        #     if currentLevel >= len(res):
-        #         res.append(node.val)
-        #     if node.right:
-        #         dfs(node.right, currentLevel+1)
-        #     if node.left:
-        #         dfs(node.left, currentLevel+1)
+        res = []
 
-        # dfs(root, 0)
-        # return res
+        def dfs(node, i):
+            if not node:
+                return
+            if i == len(res):
+                res.append(node.val)
+            dfs(node.right, i + 1)
+            dfs(node.left, i + 1)
+
+        dfs(root, 0)
+        return res
 
         # BFS
-        res = []
-        q = deque([root])
-        while root and q:
-            res.append(q[-1].val)
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-        return res
+        # res = []
+        # q = deque([root])
+        # while root and q:
+        #     res.append(q[-1].val)
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        # return res
