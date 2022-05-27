e0f213c95e336c64a1bb6db671c38de2a864d2d5
Harry <harry1.liu@bmo.com>
2022-05-27 18:49:25 -0400

    refactor: largest rectangle in histogram variable remaming

--- array/largestRectangleInHistogram.py
+++ array/largestRectangleInHistogram.py
@@ -8,18 +8,12 @@ class Solution:
 
         for i, h in enumerate(heights):
             start = i
-
-            # When previous height is smaller , ,
             while stack and stack[-1][1] > h:
-                # pop previous value
-                previousIndex, previousHeight = stack.pop()
-                # calc new res
-                res = max(res, previousHeight * (i - previousIndex))
-                # change start variable
-                start = previousIndex
+                pi, ph = stack.pop()
+                start = pi
+                res = max(res, ph * (i - start))
             stack.append((start, h))
 
-        # compare maxes of values left in stack
         while stack:
             index, height = stack.pop()
             res = max(res, height * (len(heights) - index))
