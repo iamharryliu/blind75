cde729956a64a459521ba770e93f029a7c76db48
Harry <harry1.liu@bmo.com>
2022-06-08 14:36:23 -0400

    refactor: alternate solution for max subarray

--- array/maximumSubarray.py
+++ array/maximumSubarray.py
@@ -1,10 +1,21 @@
+import math
+
+
 class Solution:
     @classmethod
     def maxSubArray(self, nums):
-        for i, num in enumerate(nums):
-            if i > 0:
-                nums[i] = max(num, num + nums[i - 1])
-        return max(nums)
+        curr = 0
+        res = -math.inf
+        for num in nums:
+            curr = max(curr + num, num)
+            res = max(res, curr)
+        return res
+
+        # Alternate solution
+        # for i, num in enumerate(nums):
+        #     if i > 0:
+        #         nums[i] = max(num, num + nums[i - 1])
+        # return max(nums)
 
 
 # [-2, 1, -3, 4, -1, 2, 1, -5, 4]
