c48db61adb21cbff9e4112c511defe588e56b441
Harry <harryliu1995@gmail.com>
2022-06-13 12:04:21 -0400

    refactor: course schedule

--- graph/courseSchedule.py
+++ graph/courseSchedule.py
@@ -1,23 +1,24 @@
+from collections import defaultdict
+
+
 class Solution:
     @classmethod
     def canFinish(self, numCourses, prerequisites):
-        adj_list = {i: [] for i in range(numCourses)}
+        graph = defaultdict(list)
         for a, b in prerequisites:
-            adj_list[a].append(b)
+            graph[a].append(b)
         visited = set()
 
         def dfs(course):
             if course in visited:
                 return False
-            if adj_list[course] == []:
+            if graph[course] == []:
                 return True
-
             visited.add(course)
-            for prereq in adj_list[course]:
-                if not dfs(prereq):
+            for pre in graph[course]:
+                if not dfs(pre):
                     return False
-            visited.remove(course)
-            adj_list[course] = []
+            graph[course] = []
             return True
 
         for course in range(numCourses):
