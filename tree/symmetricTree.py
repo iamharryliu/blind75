550f69a4867ef23976d062a6268ef6eed2cad3f3
Harry <harryliu1995@gmail.com>
2022-06-12 23:55:45 -0400

    q: symmetric tree using recursion

---
+++ tree/symmetricTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(l, r):
+            if not l and not r:
+                return True
+            if l and r and l.val == r.val:
+                return dfs(l.left, r.right) and dfs(l.right, r.left)
+            else:
+                return False
+
+        return dfs(root, root)
