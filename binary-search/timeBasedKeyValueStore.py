2025f338628c865a3369e6be7fecf6cd5dc8de95
Harry <harry1.liu@bmo.com>
2022-06-13 15:43:37 -0400

    refactor: time based key value store

--- binary-search/timeBasedKeyValueStore.py
+++ binary-search/timeBasedKeyValueStore.py
@@ -3,24 +3,25 @@ from collections import defaultdict
 
 class TimeMap:
     def __init__(self):
-        self.store = defaultdict(list)
+        self.hmap = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key].append([value, timestamp])
+        self.hmap[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
-        values = self.store[key]
+        values = self.hmap[key]
         l = 0
         r = len(values) - 1
         while l <= r:
             m = (l + r) // 2
-            if values[m][1] == timestamp:
+            t = values[m][1]
+            if t == timestamp:
                 return values[m][0]
-            if values[m][1] < timestamp:
-                res = values[m][0]
+            if t < timestamp:
                 l = m + 1
-            if values[m][1] > timestamp:
+                res = values[m][0]
+            if t > timestamp:
                 r = m - 1
         return res
 
