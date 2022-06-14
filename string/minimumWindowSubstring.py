a99d559233a144960ff3d51a42ecd98b29141d31
Harry <harry1.liu@bmo.com>
2022-06-14 14:15:30 -0400

    refactor: minumum window substring

--- string/minimumWindowSubstring.py
+++ string/minimumWindowSubstring.py
@@ -4,40 +4,26 @@ import math
 class Solution:
     @classmethod
     def minWindow(self, s, t):
-        if not t:
-            return ""
-
-        count_t = {}
-        count_s = {}
+        smap = {}
+        tmap = {}
         for c in t:
-            count_t[c] = 1 + count_t.get(c, 0)
-        have, need = 0, len(count_t)
+            tmap[c] = 1 + tmap.get(c, 0)
+        have, need = 0, len(tmap)
         res = [0, math.inf]
 
         l = 0
         for r, c in enumerate(s):
-            count_s[c] = 1 + count_s.get(c, 0)
-
-            # increment have?
-            if c in count_t and count_s[c] == count_t[c]:
+            smap[c] = 1 + smap.get(c, 0)
+            if c in tmap and smap[c] == tmap[c]:
                 have += 1
-
             while have == need:
-
-                # compare curr_len and prev_len
-                curr_len = r - l + 1
                 pl, pr = res
-                prev_len = pr - pl + 1
-                if curr_len < prev_len:
+                if r - l < pr - pl:
                     res = [l, r]
-
-                # update l index and
-                count_s[s[l]] -= 1
-                if s[l] in count_t and count_s[s[l]] < count_t[s[l]]:
+                smap[s[l]] -= 1
+                if s[l] in tmap and smap[s[l]] < tmap[s[l]]:
                     have -= 1
                 l += 1
-
-        # return res string if res_len is not infinity
         l, r = res
         return s[l : r + 1] if res != [0, math.inf] else ""
 
