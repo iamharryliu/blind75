c50dbc301bd0e74aa7cfacf28ac9273c5ab5cbae
Harry <harry1.liu@bmo.com>
2022-06-14 15:50:02 -0400

    refactor: find median from data stream

--- heap/findMedianFromDataStream.py
+++ heap/findMedianFromDataStream.py
@@ -3,39 +3,27 @@ import heapq
 
 class MedianFinder:
     def __init__(self):
-        # initialize empty heaps
-        self.small, self.large = [], []
+        self.s = []
+        self.l = []
 
     def addNum(self, num: int) -> None:
-        # push num into small heap
-        heapq.heappush(self.small, -1 * num)
-
-        # if small heap root value is is greater than large heap root value move it to large heap
-        if self.small and self.large and not ((self.small[0] * -1) < self.large[0]):
-            val = heapq.heappop(self.small) * -1
-            heapq.heappush(self.large, val)
-
-        # small heap is too long
-        if len(self.large) + 1 < len(self.small):
-            val = heapq.heappop(self.small) * -1
-            heapq.heappush(self.large, val)
-
-        # large heap is too long
-        if len(self.small) + 1 < len(self.large):
-            val = heapq.heappop(self.large) * -1
-            heapq.heappush(self.small, val)
+        heapq.heappush(self.s, -num)
+        if self.s and self.l and -self.s[0] > self.l[0]:
+            num = -heapq.heappop(self.s)
+            heapq.heappush(self.l, num)
+        if len(self.s) - len(self.l) > 1:
+            num = -heapq.heappop(self.s)
+            heapq.heappush(self.l, num)
+        if len(self.l) - len(self.s) > 1:
+            num = -heapq.heappop(self.l)
+            heapq.heappush(self.s, num)
 
     def findMedian(self) -> float:
-        # return small heap root if small heap is longer
-        if len(self.large) < len(self.small):
-            return self.small[0] * -1
-
-        # return small heap root if large heap is longer
-        if len(self.small) < len(self.large):
-            return self.large[0]
-
-        # return median
-        return (-1 * self.small[0] + self.large[0]) / 2
+        if len(self.s) > len(self.l):
+            return -self.s[0]
+        if len(self.l) > len(self.s):
+            return self.l[0]
+        return (-1 * self.s[0] + self.l[0]) / 2
 
 
 commands = ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
