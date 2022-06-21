7065b7abffd6e3b41748713b85b05f9d29fdba8c
Harry <harry1.liu@bmo.com>
2022-06-21 11:24:49 -0400

    refactor: subtree of another tree

--- tree/subtreeOfAnotherTree.py
+++ tree/subtreeOfAnotherTree.py
@@ -1,11 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    def isSubtree(self, root, subRoot):
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         def isSameTree(n1, n2):
             if n1 and n2:
                 return (
@@ -17,11 +21,13 @@ class Solution:
 
         if not root:
             return False
-        if isSameTree(root, subRoot):
-            return True
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        return (
+            isSameTree(root, subRoot)
+            or self.isSubtree(root.left, subRoot)
+            or self.isSubtree(root.right, subRoot)
+        )
 
-    def isSubtree(self, s, t):
+        # Merckle
         from hashlib import sha256
 
         def hashFn(x):
